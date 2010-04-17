84be05feef47a0c7829b979286c95ad642785b0f
nachouve <nachouve@290b21c2-c244-9aa2-d9a4-e58ebe4ca443>
2010-04-17 22:59:40 +0000

    License section on the code

--- trunk/src/parsers/MATE_CSVParser.py
+++ trunk/src/parsers/MATE_CSVParser.py
@@ -1,8 +1,32 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Parser for CSV INE files (abredatos.org)
-# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 mapastematicos.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Software developed for abredatos 2010
+#
+# AUTHORS:
+# Francisco Puga Alonso <fran.puga@gmail.com> <http://conocimientoabierto.es>
+# Andres Maneiro Boga <andres.maneiro@gmail.com> <http://nosolosoftware.es>
+# Adrián Eiris
+# Nacho Varela
 
-#import csv
+## Parser for CSV INE files (abredatos.org)
+
 import getopt
 import sys, os, codecs
 
