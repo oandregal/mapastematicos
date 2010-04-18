698a3c23837bc1e67b74d6a1e021a19f4d067d15
andres.maneiro@gmail.com <andres.maneiro@gmail.com@290b21c2-c244-9aa2-d9a4-e58ebe4ca443>
2010-04-18 09:00:24 +0000

    Adding test dataset

---
+++ web/tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+db_config = {'db_host': "yourhost",
+             'db_name': "yourname",
+             'db_user': "youruser",
+             'db_pass': "yourpass"}
+
+user = {'nick' : "your_nick",
+        'email': "your_email"}
+
+tags = ["tag1", "tag2", "tag3"]
+
+map_data = {'table_csv'  : "table",
+            'column_name': "column"}
+
+report_data = {'title'          : "Title",
+               'description'    : "una descripción",
+               'units'          : "unidades",
+               'region_analysis': "CCAA",
+               'id_user'        : 0,
+               'data_source'    : "INE",
+               'footnote'       : "footnotes",
+               'data_copyright' : "copyright"}
+
